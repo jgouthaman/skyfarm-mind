3eee537ab20706cc1093cf2a3af36c2c0938d0eb
Research HQ <votingaas@gmail.com>
2026-07-01 12:29:52 +0530

    fix: use named function component in learn layout route to fix Vercel build

--- src/routes/_layout.learn.tsx
+++ src/routes/_layout.learn.tsx
@@ -1,5 +1,9 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { createFileRoute, Outlet } from '@tanstack/react-router'
 
-export const Route = createFileRoute("/_layout/learn")({
-  component: () => <Outlet />,
-});
+export const Route = createFileRoute('/_layout/learn')({
+  component: LearnLayout,
+})
+
+function LearnLayout() {
+  return <Outlet />
+}
